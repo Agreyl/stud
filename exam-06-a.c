9336dff2061adcd6b524a70d89bc0cea49422448
Agreyl <ankadan08@gmail.com>
2021-10-06 09:35:47 +0000

    na 3

--- exam-06-a.c
+++ exam-06-a.c
@@ -7,13 +7,27 @@
 int main()
 {
     int pressure[d];
-    for (int i = 0; i <= d; i++)
+    for (int i = 0; i < d; i++)
     {
         pressure[i] = min + (rand()%(max-min+1));
     }
-    for (int i = 0; i <= d; i++)
+    for (int i = 0; i < d; i++)
     {
-        printf("%d\t%d", i+1, pressure[i]);
+        printf("%d\t%d\n", i+1, pressure[i]);
     }
+    int d_below = 0;
+    int d_above = 0;
+    int d_exact = 0;
+
+    for (int i = 0; i < d; i++)
+    {
+        int v = pressure[i];
+        if (v < norm) d_below++;
+        else if (v > norm) d_above++;
+        else d_exact++;
+    }
+    printf("below: %d\n", d_below);
+    printf("above: %d\n", d_above);
+    printf("exact: %d\n", d_exact);
     return 0;
 }
