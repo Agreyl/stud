9023db48755ea8c605d00f5ae347efee0783eb0d
Agreyl <91784309+Agreyl@users.noreply.github.com>
2021-11-24 10:49:25 +0300

    na 4

--- exam-06-b.c
+++ exam-06-b.c
@@ -29,6 +29,33 @@ int main()
     printf("below: %d\n", d_below);
     printf("above: %d\n", d_above);
     printf("exact: %d\n", d_exact);
+    
+    printf("Delete day? Print '1' if yes, '0' if no.\n");
+    int g;
+    int dm;
+    scanf("%d\n", &g);
+    if (g == 1)
+    {
+        printf("Day of month:\n");
+        scanf("%d\n", &dm);
+        for (int i = 0; i < d; i++)
+        {
+            if (i == dm - 1)
+            {
+                pressure[i] = -1;
+                printf("Deleted succesfully.\n");
+
+            }            
+        }
+        for (int i = 0; i < d; i++)
+        {
+            if (pressure[i] != -1)
+            printf("%d\t%d\n", i + 1, pressure[i]);
+            else printf("%d\tDELETED\n", i + 1);
+        }
+
+    }
+    
     return 0;
     
 }
