d353cd276a7a2a1c9dba43cc1576c58fb8c4d169
Agreyl <91784309+Agreyl@users.noreply.github.com>
2021-11-25 10:58:50 +0300

    Na 5

--- exam-06-c.c
+++ exam-06-c.c
@@ -6,21 +6,23 @@
 #define norm 760
 #define max 780 
 
-int cmp(const void* a, const void* b) 
-{
-    return *(int*)a - *(int*)b;
-}
-
 int main()
 {
-    int pressure[d];
+    struct day_pressure
+    {
+        int day;
+        int pressure;
+
+    } ;
+    struct day_pressure pressure[d];
     for (int i = 0; i < d; i++)
     {
-        pressure[i] = min + (rand() % (max - min + 1));
+        pressure[i].pressure = min + (rand() % (max - min + 1));
+        pressure[i].day = i + 1;
     }
     for (int i = 0; i < d; i++)
     {
-        printf("%d\t%d\n", i + 1, pressure[i]);
+        printf("%d\t%d\n", pressure[i].day, pressure[i].pressure);
     }
 
     int d_below = 0;
@@ -29,7 +31,7 @@ int main()
 
     for (int i = 0; i < d; i++)
     {
-        int v = pressure[i];
+        int v = pressure[i].pressure;
         if (v < norm) d_below++;
         else if (v > norm) d_above++;
         else d_exact++;
@@ -42,40 +44,62 @@ int main()
     printf("Delete day? Print '1' if yes, '0' if no.\n");
     int g;
     int dm;
-    scanf("%d\n", &g);
+    scanf("%d", &g);
     if (g == 1)
     {
         printf("Day of month:\n");
-        scanf("%d\n", &dm);
+        scanf("%d", &dm);
         for (int i = 0; i < d; i++)
         {
             if (i == dm - 1)
             {
-                pressure[i] = -1;
+                pressure[i].pressure = -1;
                 printf("Deleted succesfully.\n");
 
-            }            
+            }
         }
         for (int i = 0; i < d; i++)
         {
-            if (pressure[i] != -1)
-            printf("%d\t%d\n", i + 1, pressure[i]);
-            else printf("%d\tDELETED\n", i + 1);
+            if (pressure[i].pressure != -1)
+                printf("%d\t%d\n", pressure[i].day, pressure[i].pressure);
+            else printf("%d\tDELETED\n", pressure[i].day);
         }
 
     }
 
     printf("Sort days? Print '1' if yes, '0' if no.\n");
     int s;
-    scanf("%d\n", &s);
+    scanf("%d", &s);
     if (s == 1)
     {
-        qsort(pressure, d, sizeof(int), cmp);
+
+        for (int i = 0; i < d - 1; i++)
+        {
+            for (int j = d - 1; j > i; j--)
+            {
+                if (pressure[j - 1].pressure > pressure[j].pressure)
+                {
+                    int savePress = pressure[j - 1].pressure;
+                    pressure[j - 1].pressure = pressure[j].pressure;
+                    pressure[j].pressure = savePress;
+
+                    int saveDate = pressure[j - 1].day;
+                    pressure[j - 1].day = pressure[j].day;
+                    pressure[j].day = saveDate;
+                }
+            }
+                
+        }
+            
+        
+        
+        
+        
         for (int i = 0; i < d; i++)
         {
-            if (pressure[i] != -1)
-                printf("%d\t%d\n", i + 1, pressure[i]);
-            else printf("%d\tDELETED\n", i + 1);
+            if (pressure[i].pressure != -1)
+                printf("%d\t%d\n", pressure[i].day, pressure[i].pressure);
+            else printf("%d\tDELETED\n", pressure[i].day);
         }
 
     }
@@ -83,3 +107,4 @@ int main()
 
 
     return 0;
+}
