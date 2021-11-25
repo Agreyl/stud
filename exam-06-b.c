723bb03379b9c5db8ec9098171356b819faeeff8
Agreyl <91784309+Agreyl@users.noreply.github.com>
2021-11-25 11:00:01 +0300

    Na 4

--- exam-06-b.c
+++ exam-06-b.c
@@ -6,52 +6,61 @@
 #define max 780 
 int main()
 {
-    int pressure[d];
+    struct day_pressure
+    {
+        int day;
+        int pressure;
+
+    } ;
+    struct day_pressure pressure[d];
     for (int i = 0; i < d; i++)
     {
-        pressure[i] = min + (rand()%(max-min+1));
+        pressure[i].pressure = min + (rand() % (max - min + 1));
+        pressure[i].day = i + 1;
     }
     for (int i = 0; i < d; i++)
     {
-        printf("%d\t%d\n", i+1, pressure[i]);
+        printf("%d\t%d\n", pressure[i].day, pressure[i].pressure);
     }
+
     int d_below = 0;
     int d_above = 0;
     int d_exact = 0;
 
     for (int i = 0; i < d; i++)
     {
-        int v = pressure[i];
+        int v = pressure[i].pressure;
         if (v < norm) d_below++;
         else if (v > norm) d_above++;
         else d_exact++;
     }
+
     printf("below: %d\n", d_below);
     printf("above: %d\n", d_above);
     printf("exact: %d\n", d_exact);
-    
+
     printf("Delete day? Print '1' if yes, '0' if no.\n");
     int g;
     int dm;
-    scanf("%d\n", &g);
+    scanf("%d", &g);
     if (g == 1)
     {
         printf("Day of month:\n");
-        scanf("%d\n", &dm);
+        scanf("%d", &dm);
         for (int i = 0; i < d; i++)
         {
             if (i == dm - 1)
             {
-                pressure[i] = -1;
+                pressure[i].pressure = -1;
                 printf("Deleted succesfully.\n");
 
-            }            
+            }
         }
         for (int i = 0; i < d; i++)
         {
-            if (pressure[i] != -1)
-            printf("%d\t%d\n", i + 1, pressure[i]);
-            else printf("%d\tDELETED\n", i + 1);
+            if (pressure[i].pressure != -1)
+                printf("%d\t%d\n", pressure[i].day, pressure[i].pressure);
+            else printf("%d\tDELETED\n", pressure[i].day);
         }
 
     }
