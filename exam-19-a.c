e510132b5de912dd2addecf36951666902bb4eb3
Agreyl <91784309+Agreyl@users.noreply.github.com>
2021-11-26 19:28:24 +0300

    na 3

---
+++ exam-19-a.c
@@ -0,0 +1,45 @@
+#include <stdio.h> // FILE, fopen(), fclose(), fscanf(), fgets()
+#define max_ans 5
+#define max_txt 100
+struct quest
+{
+	int nans;
+	int corr;
+	char txt[max_txt];
+	char ans[max_ans][max_txt]
+};
+
+#define max_quest 10
+struct quest test[max_quest];
+
+
+int main()
+{
+	FILE* f = fopen("extask19-in.txt", "r");
+	int nq;
+	fscanf(f, "%d", &nq);
+	for (int i = 0; i < nq; i++)
+	{
+		fscanf(f, "%d %d\n", &test[i].nans, &test[i].corr);
+		fgets(test[i].txt, max_txt, f);
+		for (int j = 0; j < test[i].nans; j++)
+			fgets(test[i].ans[j], max_txt, f);
+	}
+	fclose(f);
+	for (int i = 0; i < nq; i++)
+
+	{
+		printf(test[i].txt);
+		for (int j = 0; j < test[i].nans; j++)
+			printf(test[i].ans[j]);
+
+		int c;
+		scanf("%d", &c);
+		if (c == test[i].corr) puts("correct!");
+		else puts("wrong!");
+
+	}
+
+	return 0;
+
+}
